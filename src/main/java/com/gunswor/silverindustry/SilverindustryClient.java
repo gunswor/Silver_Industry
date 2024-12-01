0b756afed28ffa9a04e47d48913d3d9c0b049ad4
填用户名 <z1207t@126.com>
2024-12-01 22:00:26 +0800

    Initial commit

---
+++ src/main/java/com/gunswor/silverindustry/SilverindustryClient.java
@@ -0,0 +1,10 @@
+package com.gunswor.silverindustry;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SilverindustryClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
